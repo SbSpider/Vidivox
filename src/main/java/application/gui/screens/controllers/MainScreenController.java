5bd6f00aaad7906b8d087a0afc8a93cab92e0ca6
Shorya Raj <rajshorya@gmail.com>
2015-10-03 23:03:32 +1300

    Forgot to accidentally add the old codes. Changed location of several classes. Added images folder, with images. Added references.txt to keep note of links used for referencing purposes.

---
+++ src/main/java/application/gui/screens/controllers/MainScreenController.java
@@ -0,0 +1,25 @@
+package application.gui.screens.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+/**
+ * THe controller for the main screen.
+ * 
+ * @author rajsh
+ *
+ */
+public class MainScreenController implements Initializable {
+
+	/**
+	 * Initializes the screen.
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
