6e97dc542fadc137af6868538bbf91f7752cffdf
Shorya Raj <rajshorya@gmail.com>
2015-10-18 17:07:54 +1300

    Added string savable object

---
+++ src/main/java/framework/savefunction/StringSaveableObject.java
@@ -0,0 +1,23 @@
+package framework.savefunction;
+
+public class StringSaveableObject extends SaveableObject {
+
+	String value;
+
+	public StringSaveableObject() {
+	}
+
+	public StringSaveableObject(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public Object getValue() {
+		return value;
+	}
+
+	@Override
+	public void setValue(Object obj) {
+		value = (String) obj;
+	}
+}
