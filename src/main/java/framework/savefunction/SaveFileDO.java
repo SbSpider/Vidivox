ac4faee684417ff528a975d8d9a7561d6d97764a
Shorya Raj <rajshorya@gmail.com>
2015-10-18 17:04:55 +1300

    Added 'get save dir' functionality to the save object

--- src/main/java/framework/savefunction/SaveFileDO.java
+++ src/main/java/framework/savefunction/SaveFileDO.java
@@ -3,6 +3,8 @@ package framework.savefunction;
 import java.io.Serializable;
 import java.util.HashMap;
 
+import org.apache.commons.io.FilenameUtils;
+
 public class SaveFileDO implements Serializable {
 
 	public SaveFileDO() {
@@ -14,6 +16,8 @@ public class SaveFileDO implements Serializable {
 	 */
 	private String saveFilename;
 
+	private String saveDirectory;
+
 	// private Hashmap
 	private HashMap<String, SaveableObject> saveableObjects = new HashMap<String, SaveableObject>();
 
@@ -21,8 +25,14 @@ public class SaveFileDO implements Serializable {
 		return saveFilename;
 	}
 
+	public String getSaveDirectory() {
+		return saveDirectory;
+	}
+
 	public void setSaveFilename(String saveFilename) {
 		this.saveFilename = saveFilename;
+
+		this.saveDirectory = FilenameUtils.getBaseName(saveFilename);
 	}
 
 	public HashMap<String, SaveableObject> getSaveableObjects() {
